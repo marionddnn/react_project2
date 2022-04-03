790fd095aeb8a56436a039bb2e5220e3912d4882
marion dieudonné <m.j.dieudonne@orange.fr>
2022-04-03 12:12:06 +0200

    modifier les notes

--- movieRating/components/Home.js
+++ movieRating/components/Home.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { ScrollView, Image } from "react-native";
+import { ScrollView, Image, View, Text, TextInput, Button } from "react-native";
 import { useRoute, useFocusEffect } from "@react-navigation/native";
 import Film from "./Film";
 
@@ -23,11 +23,21 @@ const Home = () => {
   ];
   
     const [filmList, setFilmToList] = useState(initialList);
-  
+    const [note, setNote] = useState("");
+
     const addFilmToList = () => {
       const {title, resume, notes, link} = route.params;
       let id = filmList.length;
       setFilmToList([...filmList, {id: id, title: title, resume: resume, notes: notes, link: link}]);
+    };
+
+    const setNotesFilm = (film) => {
+        const filmUpdate = filmList.map((f) => {
+          if (f.id === film.id) {
+              return {...f, notes: note};
+          } else return f;
+        });
+        setFilmToList(filmUpdate);
     };
   
     useFocusEffect(() => {
@@ -40,6 +50,7 @@ const Home = () => {
     <>
     <ScrollView style={{ flexDirection: "row", flexWrap: "wrap"}}>
         {filmList.map((film) => (
+          <View>
           <Film
             key={film.id}
             title={film.title}
@@ -47,6 +58,15 @@ const Home = () => {
             notes={film.notes}
             link={film.link}
           ></Film>
+          <Text>Ajouter/modifier une note :</Text>
+           <TextInput
+            style={{background:"#fff", width : "fit-content", margin : "1rem"}}
+            placeholder="Notes"
+            onChangeText={setNote}
+            onSubmitEditing={() => setNotesFilm(film)}
+            value ={note}/>
+            <Button title="submit" onPress={() => setNotesFilm(film)}></Button>
+          </View>
         ))}
       </ScrollView>
     </>
